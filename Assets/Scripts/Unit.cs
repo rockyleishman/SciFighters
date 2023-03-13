f9ae2357998a093cf0b4d40903983d9b2625d2a6
Rocky Leishman <rockyleishman@gmail.com>
2023-03-13 14:42:12 -0600

    Created health system methods
    methods to be called for healing, damage, restoring health, health power-ups, health power-downs, removing health power-up and power-downs, health upgrades, health downgrades, and for resetting max health to the original amount

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    [SerializeField] public int FullHealth = 100;
-    internal int Health;
+    [SerializeField] public int BaseMaxHealth = 100;
+    protected int _permMaxHealth;
+    internal int MaxHealth { get; private protected set; }
+    internal int Health { get; private protected set; }
 
     internal Faction UnitFaction;
 
     protected Rigidbody _rigidbody;
 
     private const float GroundRayLength = 0.1f;
+    protected float unitHieght;
 
     protected bool isAlive = true;
 
-    protected float unitHieght;
-
     [SerializeField] public float MovementSpeed = 5.0f;
+    [SerializeField] public float SprintSpeed = 7.5f;
     [SerializeField] public float JumpVelocity = 5.0f;
 
     protected void Start()
@@ -25,6 +27,16 @@ public class Unit : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
 
         unitHieght = GetComponent<CapsuleCollider>().height;
+
+        //initialize unit health
+        _permMaxHealth = BaseMaxHealth;
+        MaxHealth = _permMaxHealth;
+        Health = MaxHealth;
+
+        if (BaseMaxHealth <= 0)
+        {
+            throw new System.Exception("Base Max Health must be positive!");
+        }
     }
 
     protected bool IsGrounded()
@@ -34,5 +46,134 @@ public class Unit : MonoBehaviour
         return Physics.Raycast(origin, Vector3.down, GroundRayLength, LayerMask.GetMask("Ground"));
     }
 
-    
+    #region Health System Methods
+
+    //to be called when healing damage
+    public void Heal(int healAmount)
+    {
+        Health += healAmount;
+
+        if (Health > MaxHealth)
+        {
+            Health = MaxHealth;
+        }
+    }
+
+    //to be called when taking damage
+    public void Damage(int damageAmount)
+    {
+        Health -= damageAmount;
+
+        if (Health <= 0)
+        {
+            Health = 0;
+            isAlive = false;
+        }
+    }
+
+    //to be called to restore all health
+    public void RestoreHealth()
+    {
+        Health = MaxHealth;
+    }
+
+    //to be called for max health power-ups
+    public void IncMaxHealth(int increaseAmount)
+    {
+        MaxHealth += increaseAmount;
+        
+        if (MaxHealth < 0)
+        {
+            throw new System.Exception("Max Health Overflow!");
+        }
+
+        //keep damage amount the same
+        Heal(increaseAmount);
+    }
+
+    //to be called for max health power-downs
+    public void DecMaxHealth(int decreaseAmount)
+    {
+        MaxHealth -= decreaseAmount;
+
+        if (MaxHealth < 0)
+        {
+            MaxHealth = 0;
+        }
+
+        //adjust health to new limits
+        Heal(0);
+        Damage(0);
+    }
+
+    //to be called to remove all max health power-ups and power-downs
+    public void RevertMaxHealth()
+    {
+        MaxHealth = _permMaxHealth;
+
+        if (MaxHealth < 0)
+        {
+            throw new System.Exception("Max Health Overflow!");
+        }
+
+        //adjust health to new limits
+        Heal(0);
+        Damage(0);
+    }
+
+    //to be called for max health upgrades
+    public void IncPermMaxHealth(int increaseAmount)
+    {
+        _permMaxHealth += increaseAmount;
+        MaxHealth += increaseAmount;
+
+        if (MaxHealth < 0)
+        {
+            throw new System.Exception("Max Health Overflow!");
+        }
+        if (_permMaxHealth < 0)
+        {
+            throw new System.Exception("Perm Max Health Overflow!");
+        }
+
+        //keep damage amount the same
+        Heal(increaseAmount);
+    }
+
+    //to be called for max health downgrades
+    public void DecPermMaxHealth(int decreaseAmount)
+    {
+        _permMaxHealth -= decreaseAmount;
+        MaxHealth -= decreaseAmount;
+
+        if (MaxHealth < 0)
+        {
+            MaxHealth = 0;
+        }
+        if (_permMaxHealth < 0)
+        {
+            _permMaxHealth = 0;
+        }
+
+        //adjust health to new limits
+        Heal(0);
+        Damage(0);
+    }
+
+    //to be called to remove all max health power-ups, power-downs, upgrades, and downgrades
+    public void RevertPermMaxHealth()
+    {
+        _permMaxHealth = BaseMaxHealth;
+        MaxHealth = BaseMaxHealth;
+
+        if (BaseMaxHealth <= 0)
+        {
+            throw new System.Exception("Base Max Health must be positive!");
+        }
+
+        //adjust health to new limits
+        Heal(0);
+    }
+
+    #endregion
 }

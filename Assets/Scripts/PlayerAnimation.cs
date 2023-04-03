9eab54639a5cbc2b7100018310b8315ce1a333e6
Unity <aaron.zml331@gmail.com>
2023-04-03 17:08:00 -0600

    update animation play

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator ani;
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input.GetAxis("Vertical");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani= GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        ani.SetFloat("Horizontal", horizontalInput);
+        ani.SetFloat("Vertical", verticalInput);
+
+    }
+}

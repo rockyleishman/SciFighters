5c15afbbdf5491a721578b96923beef41c098eda
Aaron <aaron.zml331@gmail.com>
2023-04-04 02:13:11 -0600

    add run, walk, crouch animation state

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -17,10 +17,110 @@ public class PlayerAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        ani.SetFloat("Horizontal", horizontalInput);
-        ani.SetFloat("Vertical", verticalInput);
+
+        if (ani.GetBool("Crouch") == false)  //when the state is walking, we can set it to crouch state or just use walk function
+        {
+
+
+
+
+
+
+
+
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                ani.SetBool("Crouch", true);
+                
+            }
+
+            else
+            {
+                walk();
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Run();
+                }
+            }
+            
+        }
+        else  // if repress the ctrl key, set it back to walk state
+        {
+            Crouch();
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                ani.SetBool("Crouch", false);
+            }
+        }
+        if (ani.GetBool("Run") == false)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift)) //if press shift, set it to run state
+            {
+                ani.SetBool("Run", true);
+            }
+
+        }
+        else  //if it is in running state, pressing shift set it to walk state
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                ani.SetBool("Run", false);
+            }
+        }
+
+
+    }
+    void walk()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))   //if we press WSAD key, it will make walk blend tree
+        {
+
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            if (horizontalInput > 1)
+            {
+                horizontalInput=1;
+            }
+            if (verticalInput > 1)
+            {
+                verticalInput=1;
+            }
+            ani.SetFloat("Horizontal", horizontalInput, 0.05f, Time.deltaTime);
+            ani.SetFloat("Vertical", verticalInput, 0.05f, Time.deltaTime);
+        }
+        else
+        {
+            ani.SetFloat("Horizontal", 0, 0.05f, Time.deltaTime);   //set the movement to Idle gradually
+            ani.SetFloat("Vertical", 0, 0.05f, Time.deltaTime);
+        }
+    }
+    void Crouch()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            if (horizontalInput > 1)
+            {
+                horizontalInput = 1;
+            }
+            if (verticalInput > 1)
+            {
+                verticalInput = 1;
+            }
+            ani.SetFloat("Horizontal", horizontalInput, 0.05f, Time.deltaTime);
+            ani.SetFloat("Vertical", verticalInput, 0.05f, Time.deltaTime);
+        }
+        else
+        {
+            ani.SetFloat("Horizontal", 0, 0.05f, Time.deltaTime);   //set the movement to Idle gradually
+            ani.SetFloat("Vertical", 0, 0.05f, Time.deltaTime);
+        }
+    }
+    void Run()
+    {
+
+            ani.SetTrigger("Run");
 
     }
 }
